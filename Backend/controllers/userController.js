0e5468f5cc1723929dc9647674f378a49d70c2e9
Saket <saketsrivastava1245@gmail.com>
2024-07-17 10:55:01 +0530

    bcrypt

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,7 +1,8 @@
 import userModel from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 // import bcrypt from "bcrypt";
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
+import bcrypt from 'bcryptjs';
 import validator from "validator";
 
 // token generation using jwt
